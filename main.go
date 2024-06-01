c67300dd73d2e4c4210f7f93d82fe4c5408be52d
Alikhan <successstaffs@gmail.com>
2024-06-01 18:48:06 +0500

    added routes and main.go

---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"github.com/alikhanMuslim/ecommerce/controllers"
+	"github.com/alikhanMuslim/ecommerce/database"
+	"github.com/alikhanMuslim/ecommerce/middleware"
+	"github.com/alikhanMuslim/ecommerce/routes"
+	"github.com/gin-gonic/gin"
+	"log"
+	"os"
+)
+
+func main() {
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		port = "8080"
+	}
+
+	app := controllers.NewApplication(database.ProductData(database.Client, "Products"), database.UserData(database.Client, "Users"))
+
+	router := gin.New()
+
+	router.Use(gin.Logger())
+
+	routes.UserRoutes(router)
+	router.Use(middleware.Authentication())
+
+	router.GET("/addtocart", app.AddToCart())
+	router.GET("removeitem", app.RemoveItem())
+	router.GET("/cartcheckout", app.BuyFromCart())
+	router.GET("/instantbuy", app.InstantBuy())
+
+	log.Fatal(router.Run(":" + port))
+}
