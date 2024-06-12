dbef1c8311a7f9260f7046db1f38eebf01afaddd
Alikhan <successstaffs@gmail.com>
2024-06-12 16:28:19 +0500

    controllers update login and sign up

--- database/cart.go
+++ database/cart.go
@@ -1,1 +1,32 @@
 package database
+
+import (
+	"errors"
+	"github.com/gin-gonic/gin"
+)
+
+var (
+	ErrCantFindProduct    = errors.New("can't find product")
+	ErrCantDecodeProducts = errors.New("can't decode products")
+	ErrUserIdIsNotValid   = errors.New("this user is not valid")
+	ErrCantUpdateUser     = errors.New("can't add this product to the cart")
+	ErrCantRemoveItemCart = errors.New("can't remove item cart")
+	ErrCantGetItem        = errors.New("was unable to get item")
+	ErrCantBuyCartItem    = errors.New("cannot update the purchase")
+)
+
+func AddProductToCart() gin.HandlerFunc {
+
+}
+
+func RemoveCartItem() gin.HandlerFunc {
+
+}
+
+func BuyItemFromCart() gin.HandlerFunc {
+
+}
+
+func InstantBuyer() gin.HandlerFunc {
+
+}
