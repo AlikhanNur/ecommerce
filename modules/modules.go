120f32df405c826e900cbad6d1f54133beef8d4b
Alikhan <successstaffs@gmail.com>
2024-06-19 07:18:06 +0500

    Instant buyer

--- modules/modules.go
+++ modules/modules.go
@@ -25,14 +25,14 @@ type User struct {
 type Product struct {
 	Product_ID   primitive.ObjectID `bson:"_id"`
 	Product_Name *string            `json:"product_name"`
-	Price        *uint64            `json:"price"`
+	Price        int                `json:"price"`
 	Rating       *uint8             `json:"rating"`
 	Image        *string            `json:"image"`
 }
 type ProductUser struct {
 	Product_ID   primitive.ObjectID `bson:"_id"`
 	Product_Name *string            `json:"product_name" bson:"product_name"`
-	Price        *uint64            `json:"price" bson:"price"`
+	Price        int                `json:"price" bson:"price"`
 	Rating       *uint8             `json:"rating" bson:"rating"`
 	Image        *string            `json:"image" bson:"image"`
 }
